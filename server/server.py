706df3a3e3b095045eedf17f10c1dd880cd05f7e
motionDEV1 <162386493+motionDEV1@users.noreply.github.com>
2024-03-06 00:26:09 +0000

    Made server.py

---
+++ server/server.py
@@ -0,0 +1,28 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Pixel Server</title></head>", "utf-8"))
+        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is the Pixel server up and running.</p>", "utf-8"))
+        self.wfile.write(bytes("""<p>APIv0
+                                will be updated at Beta 2</p>""", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server started at http://{hostName}:{serverPort}")
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
+    print("Server stopped.")
